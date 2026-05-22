4c899413191ff6d982f5c3c31fa21f28f0ade8d5
daniswastaken <daniswastaken@gmail.com>
2026-05-23 00:24:34 +0700

    please

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-const DEV_PORT = 2121;
-
 export default defineConfig({
 	output: 'server',
-	adapter: cloudflare(),
-	server: {
-		port: DEV_PORT,
-	},
+	adapter: cloudflare({
+		mode: 'directory',
+        functionPerRoute: false 
+	}),
 	integrations: [
 		sitemap(),
 		tailwind(),
