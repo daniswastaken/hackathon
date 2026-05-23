4300817da71d0edf1c5437e8814e8ed6cfa9108b
daniswastaken <daniswastaken@gmail.com>
2026-05-24 00:49:39 +0700

    fix(config): remove hardcoded github pages base paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,6 @@ const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
-	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-
 	output: 'server',
 	adapter: cloudflare({
 		mode: 'directory',
